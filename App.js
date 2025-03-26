cfc25b954db5e6991b92c8b8d1d7991ec7ab89e6
Marco-Ayala <marcoantona2000@gmail.com>
2025-03-25 18:26:25 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import React from "react";
-import SprintPlanning from "./components/SprintPlanning";
+import React from 'react';
+import Gallery from './components/Gallery';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
   return (
-    <div>
-      <SprintPlanning />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>Rompecabezas y Modelos a Escala</h1>
+        <Gallery />
+      </div>
+    </Provider>
   );
 }
 
